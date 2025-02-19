e5fb98aac284dc080c3f4f4375a324659c83bfb9
akhila-pynam <akhilapynam@gmail.com>
2025-02-20 03:24:23 +0530

    added file io

---
+++ Binary Tree/in-order.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+     
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+
+};
+Node* binarytree() {
+
+    int value;
+    cin >> value;
+
+    if (value == -1) return nullptr;
+
+    Node* node = new Node(value);
+    node->left = binarytree();
+    node->right = binarytree();
+    return node;
+}
+
+void inorder(Node* root){
+    if(root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    Node* root = binarytree();
+    inorder(root);
+    
+
+}
