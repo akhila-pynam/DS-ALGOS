dc2f0494b42442f70cb5998f8824eb234c930a0e
akhila-pynam <akhilapynam@gmail.com>
2025-02-19 11:13:01 +0530

    added new file btr.cpp

--- Binary Tree/binary-tree-representation.cpp
+++ Binary Tree/binary-tree-representation.cpp
@@ -1,1 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Node {
+    int data;
+    Node* right = nullptr;
+    Node* left = nullptr;
+
+    Node(int value) {
+        data = value;
+        right = nullptr;
+        left = nullptr;
+    }
+};
+
+Node* binarytree() {
+
+    int value;
+    cin >> value;
+
+    if (value == -1) return nullptr;
+
+    Node* node = new Node(value);
+    node->left = binarytree();
+    node->right = binarytree();
+    return node;
+}
+
+void Preorder(Node* root) {
+
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    Preorder(root->left);
+    Preorder(root->right);
+    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    Node* root = binarytree();
+    Preorder(root);
+    cout << endl;
+
+    return 0;
+}
