3d17e293030c8d3bf24d9ec6b1230f84cdb3b4b5
akhila-pynam <akhilapynam@gmail.com>
2025-03-01 00:51:53 +0530

    added file bst

--- Binary Search Tree/binary-search.cpp
+++ Binary Search Tree/binary-search.cpp
@@ -1,1 +1,66 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int value){
+		data = value;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+Node* binarytree(){
+	int value;
+	cin >> value;
+
+	if(value == -1) return nullptr;
+
+	Node* node = new Node(value);
+	node->left = binarytree();
+	node->right = binarytree();
+	return node;
+}
+
+Node* binarySearch(Node* root, int val){
+
+    if(root == nullptr) return nullptr;
+    
+    if(val == root->data) return root;
+
+    if(val < root->data){
+    	return binarySearch(root->left, val);
+    }
+    else{
+    	return binarySearch(root->right, val);
+    }
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+    
+    Node* root = binarytree();
+
+    int val;
+    cin >> val;
+
+    Node* result = binarySearch(root, val);
+
+    if(result){
+       cout << result->data << " ";
+    }
+    else{
+    	cout << "not found";
+    }
+    
+
+}
