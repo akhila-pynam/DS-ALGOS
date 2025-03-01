7e7e87c7da0bae1727d760927ab599fbb4a807fd
akhila-pynam <akhilapynam@gmail.com>
2025-03-01 12:59:27 +0530

    added file fcbst

---
+++ Binary Search Tree/floor-ceil-bst.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+    	data = value;
+    	left = nullptr;
+    	right = nullptr;
+    }
+};
+
+Node* binarytree(){
+
+	int value;
+	cin >> value;
+
+	if(value == -1) return nullptr;
+
+	Node* node = new Node(value);
+    node->left = binarytree();
+    node->right = binarytree();
+    return node;
+
+}
+
+pair<int, int> result(Node* root, int val){
+
+	int floor = -1;
+	int ceil = -1;
+
+	if(root == nullptr) return {};
+
+	while(root){
+		if(root->data == val){
+			floor = root->data;
+			ceil = root->data;
+			break;
+		}
+
+		if(val > root->data){
+			floor = root->data;
+			root = root->right;
+		}
+		else{
+			ceil = root->data;
+			root = root->left;
+		}
+	}
+	return {floor, ceil};
+}
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	Node* root = binarytree();
+
+    int val;
+    cin >> val;
+
+    pair<int, int> floorAndceil= result(root, val);
+
+    cout << "Floor: " << floorAndceil.first << endl;
+    cout << "Ceil : " << floorAndceil.second << endl;
+
+}
