9b579dda0cea468d4d4c9563904526fb84cd6b0e
akhila-pynam <akhilapynam@gmail.com>
2025-03-02 14:13:51 +0530

    added file dnbst

---
+++ Binary Search Tree/delete-node-bst.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* binarytree() {
+    int value;
+    cin >> value;
+
+    if (value == -1) return nullptr;
+
+    Node* node = new Node(value);
+    node->left = binarytree();
+    node->right = binarytree();
+    return node;
+}
+
+Node* FindMin(Node* root) {
+    while (root && root->left != nullptr) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int val) {
+    if (root == nullptr) return root;
+
+    if (val < root->data) {
+        root->left = deleteNode(root->left, val);
+    } 
+    else if (val > root->data) {
+        root->right = deleteNode(root->right, val);
+    } 
+
+
+    else {
+        if (root->left == nullptr && root->right == nullptr) {
+            delete root;
+            return nullptr;
+
+        } else if (root->left == nullptr) {
+            Node* temp = root;
+            root = root->right;
+            delete temp;
+
+        } else if (root->right == nullptr) {
+            Node* temp = root;
+            root = root->left;
+            delete temp;
+            
+        } else {
+            Node* temp = FindMin(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+int main() {
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    Node* root = binarytree();
+
+    int val;
+    cin >> val;
+
+    Node* result = deleteNode(root, val);
+    inorder(result);
+
+    return 0;
+}
