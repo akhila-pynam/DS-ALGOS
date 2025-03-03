99b4b3e997baed2282b26f9e133f55c48abe9c09
akhila-pynam <akhilapynam@gmail.com>
2025-03-03 13:24:49 +0530

    added file cbtisbst

---
+++ Binary Search Tree/check-bt-is-bst.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int value){
+		data = value;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+Node* binarytree(){
+	int value;
+	cin >> value; 
+
+	if(value == -1) return nullptr;
+
+	Node* node = new Node(value);
+	node->left = binarytree();
+	node->right = binarytree();
+	return node;
+}
+
+bool checkBst(Node* root, int min, int max){
+	if(root == nullptr) return true;
+
+	if(root->data <= min || root->data >= max){
+		return false;
+	}
+
+	return checkBst(root->left, min, root->data) && checkBst(root->right, root->data, max);
+}
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    Node* root = binarytree();
+
+    int min = INT_MIN;
+    int max = INT_MAX;
+    checkBst(root, min, max);
+
+    if (checkBst(root, min, max)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+}
