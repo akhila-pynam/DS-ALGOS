31ce521187a129b790ff3c49de1809e56fb6e4db
akhila-pynam <akhilapynam@gmail.com>
2025-03-06 00:06:28 +0530

    modified

---
+++ Binary Search Tree/inorder-successor.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+	}
+};
+
+Node* binarySearchtree(){
+	int value;
+	cin >> value;
+
+	if(value == -1) return nullptr;
+
+	Node* node = new Node(value);
+	node->left = binarySearchtree();
+	node->right = binarySearchtree();
+	return node;
+}
+
+Node* inorderSuccessor(Node* root, int val){
+
+	Node* Successor = nullptr;
+
+    while(root){
+	    if(val >= root->data){
+	       root = root->right;
+		}
+		else{
+          Successor = root;
+		  root = root->left;
+		}
+    }
+	return Successor;
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	Node* root = binarySearchtree();
+
+	int val;
+	cin >> val;
+
+    Node* Successor = inorderSuccessor(root, val);
+
+    if(Successor){
+    	cout << Successor->data;
+    }
+    else{
+    	cout << "No Successor";
+    }
+}
