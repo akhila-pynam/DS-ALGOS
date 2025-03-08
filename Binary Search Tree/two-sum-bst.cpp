39aae5de61ca4df685650a7b29e892b7d0bd5782
akhila-pynam <akhilapynam@gmail.com>
2025-03-08 16:26:59 +0530

    modified twosum

--- Binary Search Tree/two-sum-bst.cpp
+++ Binary Search Tree/two-sum-bst.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node{
     int data;
     Node* left;
     Node* right;
 
-    Node(int value) {
+    Node(int value){
         data = value;
         left = nullptr;
         right = nullptr;
     }
 };
 
-Node* binarySearchtree() {
+Node* binarySearchtree(){
+    
     int value;
     cin >> value;
 
@@ -23,51 +24,57 @@ Node* binarySearchtree() {
     node->left = binarySearchtree();
     node->right = binarySearchtree();
     return node;
+
 }
 
-void inorder(Node* root, vector<int>& elements) {
+void inorder(Node* root, vector<int>& elements){
+    
     if(root == nullptr) return;
 
     inorder(root->left, elements);
     elements.push_back(root->data);
     inorder(root->right, elements);
+
 }
 
-bool twoSumBST(Node* root, int target) {
+bool twoSum(Node* root, int target){
     vector<int> elements;
     inorder(root, elements);
 
     int i = 0;
     int j = elements.size() - 1;
 
-    while(i < j) {
+    while(i<j){
+
         int sum = elements[i] + elements[j];
-        if(sum == target) {
-            return true;
-        } else if(sum < target) {
+
+        if(sum == target) return true;
+
+        else if(sum < target){
             i++;
-        } else {
+        }
+        else{
             j--;
         }
     }
-    return false;
-}
 
-int main() {
+    return false;
+
+}
+int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-
     Node* root = binarySearchtree();
+
     int target;
     cin >> target;
 
-    if(twoSumBST(root, target)) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
+    if(twoSum(root , target)){
+        cout << "true";
     }
-
-    return 0;
+    else{
+        cout << "false";
+    }
 }
