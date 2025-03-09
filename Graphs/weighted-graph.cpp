e1c5d34205feaa75860de10c11cddc6e55ac63c7
akhila-pynam <akhilapynam@gmail.com>
2025-03-09 23:59:11 +0530

    modified wg

--- Graphs/weighted-graph.cpp
+++ Graphs/weighted-graph.cpp
@@ -11,7 +11,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
+	int adj[n+1][n+1] = {0};
 
     for (int i = 0; i < m; i++) {
         int u, v, w;
