17f9a064e1c3f69203114960cd11493d8da50d32
akhila-pynam <akhilapynam@gmail.com>
2025-03-09 23:56:32 +0530

    added file wg

---
+++ Graphs/weighted-graph.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        adj[u][v] = w;
+        adj[v][u] = w;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
