d83231bcedcb9c314b2206c9c64bf15a1bb52e67
akhila-pynam <akhilapynam@gmail.com>
2025-03-18 12:28:18 +0530

    added file tka

---
+++ Graphs/topo-khans-algo.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int topokhansAlgo(vector<vector<int>>& adjList, vector<int>& indegree, int n, vector<int>& topo) {
+    
+    for (int i = 0; i < n; i++) {
+        for (auto neighbor : adjList[i]) {
+            indegree[neighbor]++;
+        }
+    }
+
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+
+        for (auto neighbor : adjList[node]) {
+            indegree[neighbor]--;
+            if (indegree[neighbor] == 0) {
+                q.push(neighbor);
+            }
+        }
+    }
+
+    return topo.size() == n;
+}
+
+int main() {
+	
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adjList(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adjList[u].push_back(v);
+    }
+
+    vector<int> indegree(n, 0);
+    vector<int> topo;
+
+    topokhansAlgo(adjList, indegree, n, topo);
+
+    if (topokhansAlgo){
+        for (int node : topo) {
+            cout << node << " ";
+        }
+    }
+
+    return 0;
+}
