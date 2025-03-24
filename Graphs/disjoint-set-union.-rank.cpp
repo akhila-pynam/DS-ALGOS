e1decb4a6597ce4dc2ca719a53afbea0811eaf06
akhila-pynam <akhilapynam@gmail.com>
2025-03-24 09:41:24 +0530

    added rank

--- Graphs/disjoint-set-union.-rank.cpp
+++ Graphs/disjoint-set-union.-rank.cpp
@@ -11,26 +11,30 @@ int findParent(int node) {
 
 }
 
-void unionSets(int u, int v) {
+void unionRank(int u, int v, int n) {
 
     int ulp_u = findParent(u);
     int ulp_v = findParent(v);
 
     if (ulp_u == ulp_v) return;
 
-    
     if (Rank[ulp_u] < Rank[ulp_v]) {
         parent[ulp_u] = ulp_v;
     }
-
     else if (Rank[ulp_u] > Rank[ulp_v]) {
         parent[ulp_v] = ulp_u;
     }
-
     else {
         parent[ulp_v] = ulp_u;
         Rank[ulp_u]++;
     }
+ 
+
+    for(int i=1; i<=n; i++){
+        cout << Rank[i] << " ";
+    }
+    cout << endl;
+
 }
 
 int main() {
@@ -43,6 +47,13 @@ int main() {
     int n;
     cin >> n;
 
+    for(int i=0; i<n; i++){
+       
+        int ele1, ele2;
+        cin >> ele1 >> ele2;
+
+    }
+
     parent.resize(n + 1);
     Rank.resize(n + 1, 0);
 
@@ -50,18 +61,47 @@ int main() {
         parent[i] = i;
     }
 
-    int u, v;
-    cin >> u >> v;
-    unionSets(u, v);
-
     for(int i=1; i<=n; i++){
         cout << parent[i] << " ";
     }
     cout << endl;
 
-    for(int i=1; i<=n; i++){
-    cout << Rank[i] << " ";
+    
+    if(findParent(1) == findParent(3)) {
+        cout << "Same\n";
+    } 
+    else{
+        cout << "Not same\n";
     }
+
+    unionRank(1, 3, n);
+
+    if(findParent(1) == findParent(3)) {
+        cout << "Same\n";
+    } 
+    else{
+        cout << "Not same\n";
+    }
+
 
     return 0;
 }
+
+// INPUT : 
+
+// 6
+// 1 2
+// 2 3
+// 4 5
+// 6 7
+// 5 6
+// 3 7 
+
+
+// OUTPUT :
+
+// 1 2 3 4 5 6 
+// Not same
+// 1 0 0 0 0 0 
+// Same
+
