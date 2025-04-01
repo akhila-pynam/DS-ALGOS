859f6b8b53d63659ca6f1c88bc134ef5fd2a2531
akhila-pynam <akhilapynam@gmail.com>
2025-04-01 23:11:58 +0530

    coded get det clear in bit manipulation

---
+++ Bit Manipulation/get-set-clear.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int bitManipulation(int num, int postion){
+        
+        int get = (num >> (postion-1)) & 1 ;
+        int set = (1 << (postion-1)) | num ;
+        int clear =  (~(1 << postion-1)) & num;
+
+        cout << get << " " << set << " " << clear << endl;
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int num;
+	int position;
+
+	cin >> num >> position;
+
+	bitManipulation(num, position);	
+
+}
+
+// INPUT : 70 3
+// OUTPUT : 1 70 66
+
+
+// INPUT : 8 1 
+// OUPUT : 0 9 8 
