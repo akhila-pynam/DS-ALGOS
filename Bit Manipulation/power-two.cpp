1a5d234964b05a19ae754ddaa577c622b34e064e
akhila-pynam <akhilapynam@gmail.com>
2025-04-02 09:03:46 +0530

    added fle in graph

---
+++ Bit Manipulation/power-two.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool powerTwo(int n){
+
+  if(n<=0){
+    cout << "false";
+  }
+
+
+  if((n & (n-1)) == 0){
+    cout << "true";
+  }
+  
+  else{
+    cout << "false";
+  }
+
+}
+int main(){
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif
+
+  int n;
+  cin >> n;
+
+  powerTwo(n);
+
+
+}
