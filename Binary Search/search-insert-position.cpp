4ec9876aba0d344f63759554ebccd398f8833940
akhila-pynam <akhilapynam@gmail.com>
2025-04-07 23:02:19 +0530

    added file in BS

---
+++ Binary Search/search-insert-position.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int SearchPosition(int n, int* nums, int insert){
+    
+    int left = 0;
+    int right = n-1;
+
+    while(left <= right){
+    	
+    	int mid = (left+right)/2;
+
+    	if(nums[mid] < insert){
+    		left = mid + 1;
+    	}
+    	else{
+    		right = mid - 1;
+    	}
+
+    }
+
+    return left;
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+    int nums[n];
+	for(int i=0; i<n; i++){
+		cin >> nums[i];
+	}
+
+	int insert;
+	cin >> insert;
+
+	int result = SearchPosition(n, nums, insert);
+	cout << result;
+
+	return 0;
+}
+
+// INPUT : 
+
+// 4 
+// 1 3 5 6
+// 2 
+
+// OUTPUT : 1
