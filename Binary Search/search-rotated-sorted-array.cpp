1bb500141ce56fdd44b7cbba929eb8565fac9852
akhila-pynam <akhilapynam@gmail.com>
2025-04-09 19:36:09 +0530

    modified code in bs

--- Binary Search/search-rotated-sorted-array.cpp
+++ Binary Search/search-rotated-sorted-array.cpp
@@ -2,73 +2,74 @@
 using namespace std;
 
 int searchRotated(int n, int* nums, int target){
-   
+    
     int left = 0;
-    int right = n - 1;
+    int right = n-1;
+    int ans = -1;
 
-    while (left <= right) {
-       
-        int mid = left + (right - left) / 2;
+    while(left <= right){
 
-        if (nums[mid] == target) {
-            return mid;
+    	int mid = (left + right)/2;
+
+    	if(nums[mid] == target){
+    		return mid;
+    	}
+
+    	if(nums[left] <= nums[mid]){
+
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } 
+            else{
+                left = mid + 1;
+            }
+
         }
 
+        else{
 
-        if (nums[left] <= nums[mid]) {
+            if(nums[mid] < target && target <= nums[right]){
+                left = mid + 1;
+            } 
+            else{
+                right = mid - 1;
+            }
             
-            if (nums[left] <= target && target <= nums[mid]) {
-               
-                right = mid - 1;
-
-            } 
-            else {
-
-                left = mid + 1;
-
-            }
-
-        } 
-
-        else {
-           
-            if (nums[mid] <= target && target <= nums[right]) {
-               
-                left = mid + 1;
-
-            } 
-            else {
-               
-                right = mid - 1;
-
-            }
-
         }
     }
 
-    return -1;
+    return ans;
 }
 
+
 int main(){
-  
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
 
-    int n;
-    cin >> n;
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
 
-    int nums[n];
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+	int n;
+	cin >> n;
+
+	int nums[n];
+	for(int i=0; i<n; i++){
+		cin >> nums[i];
+	}
 
     int target;
     cin >> target;
-
+    
     int result = searchRotated(n, nums, target);
-    cout << result << endl;
+    cout << result;
 
     return 0;
 }
+
+// INPUT :
+
+// 7
+// 4 5 6 7 0 1 2 
+// 0
+
+// OUTPUT : 4
