bc67278d29ea777ca6be4d4cb01488111d0414e6
akhila-pynam <akhilapynam@gmail.com>
2025-04-09 17:55:54 +0530

    added file in bs

---
+++ Binary Search/ceil-the-floor.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> ceilThefloor(int n, int* nums, int target){
+    
+   
+    int left = 0;
+	int right = n-1;
+	int floor = -1;
+	int ceil = -1;
+
+	while(left <= right){
+		
+		int mid = (left + right)/2;
+
+        if(nums[mid] == target){
+            floor = ceil = nums[mid];
+			break;
+		}
+		if(nums[mid] > target){
+		    ceil = nums[mid];
+			right = mid - 1;
+		}
+		else{
+			floor = nums[mid];
+			left = mid + 1;
+		}
+	}
+
+    return {floor, ceil};
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+	int nums[n];
+	for(int i=0; i<n; i++){
+		cin >> nums[i];
+	}
+
+	int target;
+	cin >> target;
+
+	pair<int, int> result = ceilThefloor(n, nums, target);
+	cout << "{" << result.first << " " << result.second << "}";
+
+	return 0;
+
+}
+
+// INPUT : 
+
+// 6
+// 3 4 7 8 8 10
+// 5
+
+// OUTPUT : {4 7}
