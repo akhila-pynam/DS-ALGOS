9084dc73e481fe1ae8029bb6f3736b824a655667
akhila-pynam <akhilapynam@gmail.com>
2025-04-18 11:22:22 +0530

    modified coe in sw

--- Sliding Window & Two Pointer/longest-subarray-with-sum.cpp
+++ Sliding Window & Two Pointer/longest-subarray-with-sum.cpp
@@ -2,19 +2,88 @@
 using namespace std;
 
 int longestSubarray(int n, int* nums, int k){
-    
-    int maxSum = 0;
 
-    for(int i=0; i<n; i++){
-    	int sum = 0;
-    	for(int j=i; j<n; j++){
-    		sum = sum + nums[j];
-            if(sum <= k){
-              maxSum = max(maxSum, j-i+1);
+// BRUTAL SOLUTION / BRUTE FORCE 
+
+    // TC -> O(N^2)
+    // SC -> O(1)
+
+    // int maxLen = 0;
+
+    // for(int i=0; i<n; i++){
+    // 	int sum = 0;
+    // 	for(int j=i; j<n; j++){
+    // 		sum = sum + nums[j];
+    //         if(sum <= k){
+    //           maxLen = max(maxLen, j-i+1);
+    //         }
+    // 	}
+    // }
+    // return maxLen;
+
+
+// BETTER SOLUTION
+
+    // TC -> O(N + N)
+    // SC -> O(1)
+
+// Expand - Left
+// Shrink - Right 
+
+   // int left = 0;
+   // int right = k-1;
+   // int sum = 0;
+   // int maxLen = 0;
+
+   // while(right < n){
+        
+   //      for(int i=0; i<k; i++){
+   //         for(int j=i; j<k; j++){ 
+               
+   //              sum = sum + nums[i];
+
+   //              while(sum > k){
+   //                sum = sum - nums[left];
+   //                left++;
+   //              }
+   //              if(sum < k){
+   //                  maxLen = max(maxLen, j-i+1);
+   //                  right++;
+   //              }
+   //          }
+   //      }
+   //  }
+
+
+// OPTIMAL SOLUTION 
+   
+   // TC -> O(N)
+   // SC -> O(1)
+    
+   int left = 0;
+   int right = k-1;
+   int sum = 0;
+   int maxLen = 0;
+
+   while(right < n){
+        
+        for(int i=0; i<k; i++){
+           for(int j=i; j<k; j++){ 
+               
+                sum = sum + nums[i];
+
+                if(sum > k){
+                  sum = sum - nums[left];
+                  left++;
+                }
+                if(sum < k){
+                    maxLen = max(maxLen, j-i+1);
+                    right++;
+                }
             }
-    	}
+        }
     }
-    return maxSum;
+
 }
 
 int main(){
@@ -48,7 +117,8 @@ int main(){
 // 1 2 3 4 5 6
 // 10
 
-// OUTPUT : 4
+// OUTPUT : 4 ( BRUTAL SOLUTION )
+// OUTPUT : 9 ( BETTER AND OPTIMAL SOLUTION )
 
 // INPUT :
 
@@ -56,4 +126,5 @@ int main(){
 // -1 2 3 3 4 5 -1
 // 15
 
-// OUTPUT : 7
+// OUTPUT : 7 ( BRUTAL SOLUTION )
+// OUTPUT : 14 ( BETTER AND OPTIMAL SOLUTION )
