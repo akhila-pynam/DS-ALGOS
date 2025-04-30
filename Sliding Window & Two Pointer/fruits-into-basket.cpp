f0db378fb6dfc553a11de86d64c54b2d7cdc5bce
akhila-pynam <akhilapynam@gmail.com>
2025-04-30 09:25:53 +0530

    added file in sw

--- Sliding Window & Two Pointer/fruits-into-basket.cpp
+++ Sliding Window & Two Pointer/fruits-into-basket.cpp
@@ -1,101 +1,104 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fruitsBasket(int n, int* fruits){
+int fruitsBaskets(int n, int* fruits){
+   
+// Naive Solution 
 
-// Brutal Solution
-
+    // map<int, int> f;
     // int maxLen = 0;
-    // int length = 0;
-    
+
+
     // for(int i=0; i<n; i++){
-        
-    //     set<int> basket;
 
-    //     for(int j=i; j<n; j++){
+    // 	for(int j=i; j<n; j++){
 
-    //         basket.insert(fruits[j]);
+    // 		f[fruits[j]]++;
 
-    //         if(basket.size() > 2){
+	//     	if(f.size() > 2){
     //             break;
-    //         }
+	//     	}
 
-    //         if(basket.size() <= 2){
-    //             length = j - i + 1;
-    //             maxLen = max(length, maxLen);
-    //         }    
-    //     }
+	//     	if(f.size() <= 2){
+	//     		int length = j-i+1;
+	//     		maxLen = max(maxLen, length);
+	//     	}
+
+	//     }
+	//     f.clear();
     // }
     // return maxLen;
+
 
 // Optimal Solution
 
     int left = 0;
     int right = 0;
     int maxLen = 0;
-    int length = 0;
-    int k = 2;
-    map<int, int> basket;
+    map<int, int> f;
 
     while(right < n){
+      
+      f[fruits[right]]++;
 
-        basket[fruits[right]]++;
-
-        if(basket.size() > k){
-
-            while(basket.size() > k){
-                
-                basket[fruits[left]]--;
-
-                if(basket[fruits[left]] == 0){
-                    basket.erase(fruits[left]);
-                }
-                left++;
+      if(f.size() > 2){
+        
+        while(f.size() > 2){
+        	
+        	f[fruits[left]]--;
+        	
+        	if(f[fruits[left]] == 0){
+                f.erase(fruits[left]);
             }
+
+        	left++;
         }
-        if(basket.size() <= k){
-            length = right - left + 1;
-            maxLen = max(maxLen, length);
-        }
-        right++;
+
+      }
+
+      if(f.size() <= 2){
+      	int length = right - left + 1;
+      	maxLen = max(maxLen, length);
+      }
+      
+      right++;
 
     }
-    return maxLen;   
+    return maxLen;
 }
 
 int main(){
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
 
-    int n;
-    cin >> n;
+	int n;
+	cin >> n;
 
-    int fruits[n];
-    for(int i=0; i<n; i++){
-       cin >> fruits[i];  
-    }
+	int fruits[n];
+	for(int i=0; i<n; i++){
+		cin >> fruits[i];
+	}
 
-    int result = fruitsBasket(n, fruits);
-    cout << result;
+	int result = fruitsBaskets(n, fruits);
+	cout << result;
 
-    return 0;
+	return 0;
 
 }
 
 // INPUT :
 
-// 3
-// 1 2 1
-
-// OUTPUT : 3
-
-
-// INPUT :
-
-// 5
+// 5 
 // 1 2 3 2 2 
 
 // OUTPUT : 4
+
+// INPUT : 
+
+// 3 
+// 1 2 1 
+
+// OUTPUT : 3 
