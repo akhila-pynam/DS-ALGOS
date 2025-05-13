c7759b411198633475a013ca7d2010590dc39c3a
akhila-pynam <akhilapynam@gmail.com>
2025-05-13 20:39:24 +0530

    added file sll

---
+++ Linked List/single-linked-list.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* right;
+   
+    Node(int val){
+        data = val;
+        right = NULL;
+        
+    }
+};
+
+Node* head = NULL;
+void insert(int value){
+   
+    if(head == NULL){
+        head = new Node(value);
+    }
+    else{
+
+        Node* temp = head;
+        while(temp->right != NULL){
+            temp = temp->right;
+        }
+        temp->right = new Node(value); 
+        
+    }
+
+}
+
+void display(){
+
+    Node* temp = head;
+    while(temp->right != NULL){
+        cout << temp->data << " ";
+        temp = temp->right;
+    }
+    cout << temp->data << " ";
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int value;
+        cin >> value;
+
+        insert(value);
+
+    }
+    display();
+    return 0;
+
+}
+
+// INPUT : 
+
+// 7
+// 5 2 3 6 4 7 8 
+
+// OUTPUT : 5 2 3 6 4 7 8 
