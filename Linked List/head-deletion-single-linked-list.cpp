7b82c9d8b2a448f5d3187b8c82f6da837db89489
akhila-pynam <akhilapynam@gmail.com>
2025-05-14 11:11:04 +0530

    added file in ll

---
+++ Linked List/head-deletion-single-linked-list.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+
+	int data;
+	Node* right;
+
+	Node(int val){
+		
+		data = val;
+		right = nullptr;
+
+	}
+};
+
+Node* head = nullptr;
+void insert(int value){
+     
+    if(head == nullptr){
+       head = new Node(value);
+    }
+    else{
+
+    	Node* temp = head;
+    	while(temp->right != nullptr){
+    		temp = temp->right;
+    	}
+    	temp->right = new Node(value);
+    }
+
+}
+
+void deletion(){
+
+	Node* temp = head;
+	head = head->right;
+	delete(temp);
+
+}
+
+void display(){
+
+	Node* temp = head;
+	while(temp->right != nullptr){
+		cout << temp->data << " ";
+		temp = temp->right;
+	}
+    cout << temp->data;
+}
+
+
+int main(){
+
+	#ifndef ONLIE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+	for(int i=0; i<n; i++){
+		int value;
+		cin >> value;
+
+		insert(value);
+	}
+
+    deletion();
+	display();
+	return 0;
+
+}
+
+// INPUT : 
+
+// 7
+// 5 2 3 6 4 7 8 
+
+// OUTPUT : 2 3 6 4 7 8
