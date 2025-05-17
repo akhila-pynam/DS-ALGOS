a2cf9888412f9a9f16632a5b875a70fc7c6fadec
akhila-pynam <akhilapynam@gmail.com>
2025-05-17 16:30:40 +0530

    added file in deltion

---
+++ Linked List/Double/Deletion/head-deletion-linked-list.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int val){
+		data = val;
+		right = nullptr;
+		left = nullptr;
+	}
+};
+
+Node* head = NULL;
+void insert(int value){
+    
+    if(head == nullptr){
+    	head = new Node(value);
+    }
+    else{
+
+    	Node* temp = head;
+    	while(temp->right != nullptr){
+    		temp = temp->right;
+    	}
+    	temp->right = new Node(value);
+    	temp->right->left = temp;
+        
+    }
+
+}
+
+void deleteHead(){
+     
+    Node* temp = head;
+    head = temp->right;
+    delete(temp);
+
+}
+
+void display(){
+  
+    Node* temp = head;
+    while(temp->right != nullptr){
+    	cout << temp->data << " ";
+    	temp = temp->right;
+    }
+    cout << temp->data << " ";
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+	for(int i=0; i<n; i++){
+		int value;
+		cin >> value;
+
+		insert(value);
+
+	}
+ 
+    deleteHead();
+
+	display();
+
+	return 0;
+}
+
+// INPUT : 
+
+// 7
+// 18 2 3 6 1 4 2
+
+// OUTPUT : 2 3 6 1 4 2 
