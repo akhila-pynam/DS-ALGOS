7945a0c65b1efe62ab21769be24f846628eaba3c
akhila-pynam <akhilapynam@gmail.com>
2025-05-19 01:10:00 +0530

    added folder into ll double,new file in problems

---
+++ Linked List/Double/Problems/removing-node-from-linked-list.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int val){
+		data = val;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+Node* head = NULL;
+void insert(int value){
+
+	if(head == nullptr){
+		head = new Node(value);
+	}
+	else{
+
+		Node* temp = head;
+		while(temp->right != nullptr){
+			temp = temp->right;
+		}
+		temp->right = new Node(value);
+		temp->right->left = temp;
+	}
+}
+
+void removeNode(int nodeVal){
+
+    if(head->data == nodeVal){
+       
+       Node* temp = head;
+       head = temp->right;
+       delete(temp);
+       return; 
+       
+    }
+
+	Node* temp = head;
+	while(temp->right != nullptr && temp->right->data != nodeVal){
+		temp  = temp->right;
+	}
+	delete(temp->right);
+	temp->right = temp->right->right;
+
+}
+void display(){
+
+	Node* temp = head;
+	while(temp->right != nullptr){
+		cout << temp->data << " ";
+		temp = temp->right;
+	}
+	cout << temp->data << " ";
+
+}
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+	for(int i=0; i<n; i++){
+		int value;
+		cin >> value;
+
+		insert(value);
+	}
+
+    int nodeVal;
+    cin >> nodeVal;
+
+    removeNode(nodeVal);
+
+	display();
+
+	return 0;
+}
+
+// INPUT : 
+
+// 7
+// 4 5 6 9 18 2 3
+// 4
+
+// OUTPUT : 5 6 9 18 2 3 
