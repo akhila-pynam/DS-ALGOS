b84fc74bbb8363de964528690b0e487e200e26f1
akhila-pynam <akhilapynam@gmail.com>
2025-05-23 19:02:17 +0530

    added file in ll problems

---
+++ Linked List/Problems/sort-0's-1's-and-2's-linked-list.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+      
+    int data;
+    Node* right;
+
+    Node(int val){
+    	data = val;
+    	right = nullptr;
+    }  
+
+};
+
+
+Node* head = NULL;
+void insert(int value){
+
+    if(head == nullptr){
+    	head = new Node(value);
+    }
+    else{
+
+    	Node* temp = head;
+    	while(temp->right != nullptr){
+    		temp = temp->right;
+    	}
+    	temp->right = new Node(value);
+
+    }
+
+}
+
+void sortList(){
+ 
+    Node* temp = head;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    while(temp != nullptr){
+    	pq.push(temp->data);
+    	temp = temp->right;
+    }
+
+    temp = head;
+
+    while(pq.size() != 0){
+    	temp->data =  pq.top();
+    	pq.pop();
+        temp = temp->right;
+    }
+
+}
+
+void display(){
+
+    Node* temp = head;
+    while(temp->right != nullptr){
+    	cout << temp->data << " ";
+    	temp = temp->right;
+    }
+    cout << temp->data << " ";
+
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+	for(int i=0; i<n; i++){
+		int value;
+		cin >> value;
+
+		insert(value);
+	}
+
+	sortList();
+
+	display();
+
+	return 0;
+
+}
+
+// INPUT : 
+
+// 5
+// 1 0 2 0 1 
+
+// OUTPUT : 0 0 1 1 2
+
+
+// INPUT :
+
+// 5
+// 0 2 2 1 2 
+
+// OUTPUT : 0 1 2 2 2 
