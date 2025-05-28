4f0d4073a5f1b66a4c86605ee9a7f50f8c412a75
akhila-pynam <akhilapynam@gmail.com>
2025-05-29 00:52:08 +0530

    added file in recursion

---
+++ Recursion/fibonacci-number-recursion.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Common Method  
+
+// void fibinocci(int n){
+
+//     int f[n];
+
+// 	f[0] = 0;
+// 	f[1] = 1;
+
+// 	for(int i=2; i<n; i++){
+// 		f[i] = f[i-1] + f[i-2];
+// 	}
+
+// 	cout << f[n-1];
+
+// }
+
+
+// Recursion Method 
+
+int fibinocci(int n){
+
+    if(n <= 1) return n;
+
+    return fibinocci(n-1) + fibinocci(n-2);
+
+}
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+    cout << fibinocci(n);
+
+	return 0;
+
+}
+
+
+// INPUT : 4
+// OUTPUT : 3
+
+// INPUT : 6
+// OUTPUT : 8
