196d182e2b1b33bad4c831eb167ea01b2fa004b7
akhila-pynam <akhilapynam@gmail.com>
2025-07-01 11:17:33 +0530

    modified code

--- Recursion/print-numbers-linearly-n-1-backtracking.cpp
+++ Recursion/print-numbers-linearly-n-1-backtracking.cpp
@@ -1,31 +1,65 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void func(int n, int cnt){
+     
+//     if(cnt == 0) return;
+//     cout << cnt << " ";
+//     cnt--;
+//     func(n, cnt);
+
+// }
+
+// int main(){
+
+// 	#ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// 	#endif
+
+// 	int n;
+// 	cin >> n;
+
+// 	func(n, 100);
+
+// 	return 0;
+
+// }
+
+
+
+// Back - Tracking Method
+
 #include <bits/stdc++.h>
 using namespace std;
 
 void printNums(int i, int n){
-    
-    if(i < 1) return;
-    
-    cout << i << endl;
-    i--;
-    printNums(i, n);
-    
-}
 
+	if(i == n) return;
+
+	i++;
+	printNums(i, n);
+    cout << i << endl;
+
+
+}
 int main(){
 
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	freopen("output.txt", "w", stdout);;
 	#endif
 
 	int n;
 	cin >> n;
 
-	printNums(n, n);
+	int i = 0;
+
+	printNums(i, n);
 
 	return 0;
 
 }
 
-// INPUT : 5
-// OUTPUT : 5 4 3 2 1 
+// INPUT : 7
+// OUPTUT : 7 6 5 4 3 2 1 
