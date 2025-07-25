5df11d3bf02b8cee8d6c16ce54eabc5249a528eb
akhila-pynam <akhilapynam@gmail.com>
2025-07-25 23:31:23 +0530

    added file

---
+++ Strings/reverse-string.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(int n, string s, vector<string>& output) {
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            while (j < n) {
+                while (j < n && s[j] == ' ') j++;
+                if (j >= n) break;
+
+                j = i;
+                while (j < n && s[j] == ' ') j++;
+
+                j = i - 1;
+                output.push_back(string(1, s[j]));
+            }
+        }
+    }
+}
+
+int main() {
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    string s;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+    }
+
+    vector<string> output;
+
+    reverseString(n, s, output);
+
+    for (auto it : output) {
+        cout << it << " ";
+    }
+
+    return 0;
+}
