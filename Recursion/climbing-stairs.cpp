361154571d63fb1fab14cc31421c4cb6373eedfa
akhila-pynam <akhilapynam@gmail.com>
2025-08-12 17:03:12 +0530

    added file in recursion

---
+++ Recursion/climbing-stairs.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbingStairs(int n){
+
+    if( n <= 1 ) return 1;
+
+    int left = climbingStairs(n-1);
+    int right = climbingStairs(n-2);
+
+    return left + right;
+    
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    int output = climbingStairs(n);
+    cout << output;
+
+    return 0;
+
+}
+
+// INPUT : 3 
+// OUTPUT : 3
+
+// INPUT : 4
+// OUTPUT : 5
+
+// INPUT : 2
+// OUTPUT : 2
