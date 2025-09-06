971adb2ffe5b8ab391adee89f035894c8acf78ec
akhila-pynam <akhilapynam@gmail.com>
2025-09-06 21:52:05 +0530

    added file in recursion

---
+++ Recursion/tribonacci-number.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int tribonacci(int n){
+
+	if(n == 0) return 0;
+	if(n == 1) return 1;
+	if(n == 2) return 1;
+
+    return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3); 
+ 
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >> n;
+
+    int out = tribonacci(n);
+
+    cout << out;
+
+    return 0;
+
+}
+
+// INPUT : 4 
+// OUTPUT : 4
