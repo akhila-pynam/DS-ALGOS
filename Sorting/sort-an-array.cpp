4fae125321c9e85672f116dd0e79a9fc54b4a79b
akhila-pynam <akhilapynam@gmail.com>
2025-09-13 22:54:16 +0530

    added file in sorting

--- Sorting/sort-an-array.cpp
+++ Sorting/sort-an-array.cpp
@@ -1,31 +1,68 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void swap(int& x, int& y){
+
+// 	int temp = x;
+// 	x = y;
+// 	y = temp;
+
+// }
+
+// int sortArray(int n, vector<int>& nums){
+
+//     int lft = 0;
+//     int ryt = n-1;
+//     int mid = (lft + ryt)/2;
+
+//     while(lft < ryt){
+
+//         if(nums[lft] > nums[ryt]){
+//         	swap(nums[lft], nums[ryt]);
+//         }
+
+//         lft++;
+//         ryt--;
+
+//     }
+    
+// }
+
+// int main(){
+
+// 	#ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// 	#endif
+
+// 	int n;
+// 	cin >> n;
+
+// 	vector<int> nums(n);
+// 	for(int i=0; i<n; i++){
+// 		cin >> nums[i];
+// 	}
+
+// 	int out = sortArray(n, nums);
+
+// 	for(int it : nums){
+// 		cout << it << " ";
+// 	}
+
+// 	return 0;
+
+// }
+
+
+// This Code Is Sorted By Simple Code 
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void swap(int& x, int& y){
-
-	int temp = x;
-	x = y;
-	y = temp;
-
-}
-
 int sortArray(int n, vector<int>& nums){
-
-    int lft = 0;
-    int ryt = n-1;
-    int mid = (lft + ryt)/2;
-
-    while(lft < ryt){
-
-        if(nums[lft] > nums[ryt]){
-        	swap(nums[lft], nums[ryt]);
-        }
-
-        lft++;
-        ryt--;
-
-    }
     
+    sort(nums.begin(), nums.end());
+
 }
 
 int main(){
@@ -43,19 +80,17 @@ int main(){
 		cin >> nums[i];
 	}
 
-	int out = sortArray(n, nums);
+    sortArray(n, nums);
 
-	for(int it : nums){
-		cout << it << " ";
-	}
-
+    for(int it : nums){
+    	cout << it << " ";
+    }
+ 
 	return 0;
-
 }
 
-// INPUT :
+// INPUT : 5 1 1 2 0 0 
+// OUTPUT : 0 0 1 1 2 5
 
-// 4
-// 5 2 3 1 
-
+// INPUT : 5 3 2 1 
 // OUTPUT : 1 2 3 5 
