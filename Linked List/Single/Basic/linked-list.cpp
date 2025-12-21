bfef73bd1d9d2173d9c65ab76b89351153a2257a
akhila-pynam <akhilapynam@gmail.com>
2025-12-21 19:13:11 +0530

    re-modified linked list code

--- Linked List/Single/Basic/linked-list.cpp
+++ Linked List/Single/Basic/linked-list.cpp
@@ -1,55 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// struct Node{
-
-// 	int data;
-// 	int right;
-
-// 	Node(int val){
-// 		data = val;
-// 		right = nullptr;
-// 	}
-
-// };
-
-class Node{
-public:
+struct Node
+{
 
 	int data;
-	Node* right;
-    
-    public:
-	Node(int val){
+	Node *right;
+
+	Node(int val)
+	{
 		data = val;
 		right = nullptr;
 	}
-
 };
 
-int main(){
+Node *head = NULL;
+void linkedList(int value)
+{
 
-	#ifndef ONLIE_JUDGE
+	Node *newNode = new Node(value);
+	if (head == nullptr)
+	{
+		head = newNode;
+		return;
+	}
+
+	Node *temp = head;
+	while (temp->right != nullptr)
+	{
+		temp = temp->right;
+	}
+
+	temp->right = newNode;
+}
+
+void display()
+{
+
+	Node *temp = head;
+	while (temp->right != nullptr)
+	{
+		cout << temp->data << " ";
+		temp = temp->right;
+	}
+
+	cout << temp->data << " ";
+}
+
+int main()
+{
+
+#ifndef ONLIE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	#endif
+#endif
 
 	int n;
 	cin >> n;
 
-	int nums[n];
-	for(int i=0; i<n; i++){
-		cin >> nums[i];
-	}
-    
-    Node* y = new Node(nums[2]);
-    // cout << *y; -> Here Pointer Won't Work ( Operator<< Is Not Overloaded For Node Objects )
-    cout << y->data;
-}
+	for (int i = 0; i < n; i++)
+	{
 
-// INPUT : 
+		int value;
+		cin >> value;
+
+		linkedList(value);
+	}
+
+	display();
+
+	return 0;
+}
+// INPUT :
 
 // 4
 // 2 3 4 5
 
-// OUTPUT : 2
+// OUTPUT : 2 3 4 5
