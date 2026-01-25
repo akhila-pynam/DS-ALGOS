5202fdd33c05288a7eb208774f5f6eac7102f18b
akhila-pynam <akhilapynam@gmail.com>
2026-01-25 11:56:42 +0530

    added code

--- Linked List/Problems/linked-list-cycle.cpp
+++ Linked List/Problems/linked-list-cycle.cpp
@@ -1,122 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int pos = -1;
 
 struct Node{
 
-	int data;
-	Node* right;
+    int data;
+    Node* next;
 
-	Node(int val){
-		data = val;
-		right = nullptr;
-	}
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+
 };
 
 Node* head = NULL;
-void insert(int value){
+void linkedList(int value){
 
-	if(head == nullptr){
-		head = new Node(value);
-	}
-	else{
-
-		Node* temp = head;
-		while(temp->right != nullptr){
-			temp = temp->right;
-		}
-		temp->right = new Node(value);
-
-	}
-}
-
-void cycleLinkedlist(int pos){
-
-	if(pos == -1) return;
-
-	Node* temp =  head;
-	Node* cycleNode = nullptr;
-	int index = 0;
-
-	while(temp->right != nullptr){
-
-		if(index == pos){
-			cycleNode = temp;
-		}
-
-		temp = temp->right;
-		index++;
-
-	}
-
-	if(cycleNode != nullptr){
-		temp->right = cycleNode;
-	}
-
-}
-
-bool detectCycle(){
-
-    Node* slow = head;
-    Node* fast = head;
-
-    while(fast != nullptr && fast->right != nullptr){
-    	slow = slow->right;
-    	fast = fast->right->right;
-
-    	if(slow == fast){
-    		return true;
-    	}
-
+    if(head == NULL){
+        head = new Node(value);
+        return;
     }
+
+    Node* temp = head;
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+
+    temp->next = new Node(value);
+    
+}
+
+int cnt = 0;
+
+bool LLcycle(){
+
+    map<Node*, int> mp;
+    Node* temp = head;
+    while(temp != null){
+        mp[temp] = 1;
+        temp = temp->next;
+
+        if(mp[temp] > 1) return true;
+    }
+
     return false;
+
 }
 
 int main(){
 
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	for(int i=0; i<n; i++){
-		int value;
-		cin >> value;
+    for(int i=0; i<n; i++){
 
-		insert(value);
-	}
+        int value;
+        cin >> value;
 
-	int pos;
-	cin >> pos;
+        linkedList(value);
+
+    }
+
+    bool cycle = LLcycle();
+    if(cycle){
+        cout << "true";
+    }
+
+    else cout << "false";
     
-    cycleLinkedlist(pos);
-
-    if(detectCycle()){
-    	cout << "true";
-    }
-    else{
-    	cout << "false";
-    }
-
-	return 0;
+    return 0;
 
 }
-
-// INPUT : 
-
-// 4
-// 3 2 0 -4
-// 1
-
-// OUTPUT :  true
-
-// INPUT : 
-
-// 1 
-// 1 
-// -1
-
-// OUTPUT : false
