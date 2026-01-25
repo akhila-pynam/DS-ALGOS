93fc2c8f48d3064240422d29cfd329e088e76f18
akhila-pynam <akhilapynam@gmail.com>
2026-01-25 23:45:17 +0530

    added code

--- Linked List/Problems/linked-list-cycle.cpp
+++ Linked List/Problems/linked-list-cycle.cpp
@@ -33,16 +33,39 @@ void linkedList(int value){
 }
 
 int cnt = 0;
+Node* cycleNode = NULL;
+
+void cycleDetect(int pos){
+
+    int index = 0;
+
+    if(pos != -1){
+
+        Node* temp = head;
+        while(temp->next != nullptr){
+            if(index == pos) cycleNode = temp;
+            temp = temp->next;
+            index++;
+        }
+        
+        if(index == pos) cycleNode = temp;
+        temp->next = cycleNode;
+
+    }
+
+}
 
 bool LLcycle(){
 
     map<Node*, int> mp;
     Node* temp = head;
-    while(temp != null){
+    while(temp != nullptr){
+
+        if(mp[temp] == 1) return true;
+
         mp[temp] = 1;
         temp = temp->next;
 
-        if(mp[temp] > 1) return true;
     }
 
     return false;
@@ -67,6 +90,11 @@ int main(){
         linkedList(value);
 
     }
+
+    int pos;
+    cin >> pos;
+
+    cycleDetect(pos);
 
     bool cycle = LLcycle();
     if(cycle){
