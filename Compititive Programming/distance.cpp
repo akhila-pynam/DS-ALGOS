18f472a9dc05f2db76e28771aada2520025f5528
akhila-pynam <akhilapynam@gmail.com>
2026-02-02 23:02:01 +0530

    added new folder and code

---
+++ Compititive Programming/distance.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long distance(long long x, long long y, long long a, long long b){
+
+    
+// Manhattan-Distance 
+
+    // int hDis = abs(x-a);
+    // int vDis = abs(y-b);
+    
+    // int tDis = hDis + vDis;
+
+    // return tDis;
+
+
+ // Euclidean Distance 
+
+    // return sqrt((x-a)*(x-a) + (y-b)*(y-b));
+
+// Sqrd Euclidean Distance - DSA 
+   
+    return ((x-a)*(x-a) + (y-b)*(y-b));
+    
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	long long x, y;
+	cin >> x >> y;
+
+	long long a, p;
+	cin >> a >> p;
+
+	long long out = distance(x, y, a, p);
+
+    cout << out;
+    
+	return 0;
+
+}
